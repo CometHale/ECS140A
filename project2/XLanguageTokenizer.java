a160af4071361460e353a60efbd4329466856d50
Haley Sanders <haleyfsanders@gmail.com>
2017-04-20 02:34:35 -0700

    edited Makefile and tgz for project1, added skeleton for first part of project2

---
+++ project2/XLanguageTokenizer.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xlanguageparser;
+
+/**
+ *
+ * @author Haley Sanders-Turner & Sally Ly 
+ */
+
+//for Scope: have a class for a Function token (probably not in this file)
+//each Function will have a hash table to keep track of the variables that are in scope
+//for it.
+//Or make a separate list of hash tables with one hash table for each function
+
+
+public class XLanguageTokenizer {
+    
+    public static String GetNextToken(){
+           
+        return "a";
+    }
+    
+    public static String PeekNextToken(){
+           
+        return "a";
+    }
+}
