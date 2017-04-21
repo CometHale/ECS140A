f9ecfba880aaf50cdee408934ae93906b05a1fe7
Haley Sanders <haleyfsanders@gmail.com>
2017-04-21 01:55:27 -0700

    Moved everything to the XLanguageParser file so it'll be slightly easier to develop until all the classes are finished, XLanguageTokenizer almost finished

--- project2/XLanguageParser.java
+++ project2/XLanguageParser.java
@@ -3,13 +3,113 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package xlanguageparser;
-
+//package xlanguageparser;
+import java.io.File; // Needed for XLanguageTokenizer
+import java.io.FileInputStream; // Needed for XLanguageTokenizer
+import java.io.IOException;// Needed for XLanguageTokenizer
+import java.lang.*;// Needed for XLanguageTokenizer
+import java.util.*;
 /**
  *
  * @author Haley Sanders-Turner & Sally Ly 
  */
 
+//for Scope: have a class for a Function token (probably not in this file)
+//each Function will have a hash table to keep track of the variables that are in scope
+//for it.
+//Or make a separate list of hash tables with one hash table for each function
+
+
+class XToken{
+    
+    enum TokenType { XOperator, intConstant, floatConstant, XKeywords, XIdentifiers };
+    
+    TokenType type;
+    int line_num; // need to figure out how to tell what line
+    int char_position;
+    String token;
+    
+    public XToken(){
+        line_num = -1;
+        token = "";
+    }
+    
+}//Class to gather info about a particular kind of token
+// enums restrict the possible values of an attribute to the predefined list
+
+class XLanguageTokenizer {
+    
+    File languageFile;
+    FileInputStream fis;
+    Vector token_vector;
+    
+    
+    public XLanguageTokenizer(String languageFileName) {
+        
+        languageFile = new File(languageFileName);
+        token_vector = new Vector();
+        
+        try{
+            fis = new FileInputStream(languageFile);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+                
+    }
+    
+    public String GetNextToken(){
+        
+        try{
+            
+            int fileByte = 0;
+            String tokenString = "";
+            
+            while( fileByte != -1 && !Character.isWhitespace((char)fileByte) ){
+                
+                fileByte = fis.read();
+                tokenString = tokenString + (char)fileByte;   
+            }
+            
+            XToken newToken = new XToken();
+            newToken.token = tokenString;
+//            newToken.line_num
+//            newToken.char_position
+            System.out.println(tokenString);
+            return tokenString;
+            
+        } catch(IOException e) {
+            e.printStackTrace();
+        } 
+        
+        return "";
+    }
+
+    public void PeekNextToken(){
+        
+//        try{
+//            
+//            int fileByte = 0;
+//            String tokenString = "";
+//            
+//            while( fileByte != -1 && !Character.isWhitespace((char)fileByte) ){
+//                
+//                fileByte = fis.read();
+//                tokenString = tokenString + (char)fileByte;   
+//            }
+//            
+//            XToken newToken = new XToken();
+//            newToken.token = tokenString;
+////            newToken.line_num
+////            newToken.char_position
+//            System.out.println(tokenString);
+//            
+//        } catch(IOException e) {
+//            e.printStackTrace();
+//        }  
+
+    }
+    
+}
 
 public class XLanguageParser {
 
@@ -17,7 +117,10 @@ public class XLanguageParser {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        String fileName = args[0];
+        XLanguageTokenizer tokenizer = new XLanguageTokenizer(fileName);
+        
         
     }
     
