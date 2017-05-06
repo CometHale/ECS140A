b8c90033a8aabc6960f379539ceec1d826368b39
Haley Sanders <haleyfsanders@gmail.com>
2017-05-05 18:08:33 -0700

    fixes to CParser

--- project2/CParser.java
+++ project2/CParser.java
@@ -4,75 +4,64 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class CParser {//    Based on pages 228 and 229 in "Programming Languages, Principles and Practice"
-
-    public static Tree<CToken> parseTree;
-    public static Stack<Tree.Node<CToken>> parentNodes; //nodes that are still having children added to them
-    
-    public static boolean functionDeclaration(FileInputStream f){
-        //FunctionDeclaration := ParameterBlock ;
-        
-        return true;
-    }
     
     public static boolean dataDeclaration(FileInputStream f){
         //DataDeclaration := ;
+        CScanner.getNextToken(f);
         
         if(!CScanner.prevToken.token.equals(";")){
             System.err.format("Data Declaration rule broken @%4d .",CScanner.prevToken.lineNum);
             return false;
         }
         
-        Tree.Node<CToken> newNode = new Tree.Node<CToken>(CScanner.prevToken,parentNodes.pop());
-        parseTree.insert(newNode);
-        parentNodes.push(newNode.getNodeParent()); //parent may have more children
-        
-        CScanner.getNextToken(f);
         return true;
     }
     
     public static boolean instanceType(FileInputStream f){
         //InstanceType := instance Identifier
         
+        CScanner.getNextToken(f);
+        
         if(!CScanner.prevToken.token.equals("instance")){
             System.err.format("Instance Type rule broken @%4d .",CScanner.prevToken.lineNum);
             return false;
         }
                 
-        Tree.Node<CToken> newNode = new Tree.Node<CToken>(CScanner.prevToken,parentNodes.pop());
-        parseTree.insert(newNode);
-        parentNodes.push(newNode.getNodeParent()); //parent may have more children
-        parentNodes.push(newNode);
-                
         //instance
-        if(!CScanner.prevToken.token.equals("instance")){
-            CScanner.getNextToken(f);
+        if(CScanner.prevToken.token.equals("instance")){
+            CScanner.peekNextToken(f);
         }
         
         //identifier
-//        boolean result = identifier(f);
-        
-        CScanner.getNextToken(f);
+        if(!CScanner.peekedToken.type.equals("Identifier")){
+            System.err.format("Instance Type rule broken @%4d .",CScanner.prevToken.lineNum);
+            return false;
+        }
+        else{
+//           if(!identifier(f)){
+//                return false;
+//            } 
+        }
+       
         return true;
     }
     
     public static boolean floatType(FileInputStream f){
         //FloatType := float | double
+        CScanner.getNextToken(f);
         
         if(!CScanner.prevToken.token.equals("float") || !CScanner.prevToken.token.equals("double")){
             System.err.format("Float Type rule broken @%4d .",CScanner.prevToken.lineNum);
             return false;
         }
-                
-        Tree.Node<CToken> newNode = new Tree.Node<CToken>(CScanner.prevToken,parentNodes.pop());
-        parentNodes.push(newNode.getNodeParent()); //parent may have more children
        
-        parseTree.insert(newNode);
-        CScanner.getNextToken(f);
         return true;
     }
     
     public static boolean integerType(FileInputStream f){
         //IntegerType := [unsigned] ( char | short | int | long )
+        CScanner.getNextToken(f);
+        
         String[] types = {"char","short","int","long"};
         
         if(!CScanner.prevToken.token.equals("unsigned") || !Arrays.asList(types).contains(CScanner.prevToken.token)){
@@ -80,14 +69,11 @@ public class CParser {//    Based on pages 228 and 229 in "Programming Languages
             return false;
         }
         
-        Tree.Node<CToken> newNode = new Tree.Node<CToken>(CScanner.prevToken,parentNodes.pop());
-        parentNodes.push(newNode.getNodeParent()); //parent may have more children
-        String fullToken = ""; // combine the unsigned with (char | short | int | long)
-        CToken newToken;
+//        String fullToken = ""; // combine the unsigned with (char | short | int | long)
         
         //unsigned (optional)
         if(CScanner.prevToken.token.equals("unsigned")){
-            fullToken += "unsigned ";
+//            fullToken += "unsigned ";
             CScanner.getNextToken(f);
         }
         
@@ -96,18 +82,13 @@ public class CParser {//    Based on pages 228 and 229 in "Programming Languages
             System.err.format("Integer Type rule broken @%4d .",CScanner.prevToken.lineNum);
             return false;
         }
-        else{
-            fullToken += CScanner.prevToken.token;
-            newToken = new CToken(fullToken,"Identifier", CScanner.prevToken.lineNum,CScanner.prevToken.colNum);
-            newNode.setNodeData(newToken);
-        }
         
-        parseTree.insert(newNode);
-        CScanner.getNextToken(f);
         return true;
     }
     
     public static boolean dataType(FileInputStream f){
+       
+        CScanner.getNextToken(f);
         
         String[] integerTypes = {"char","short","int","long"};
         String[] floatTypes = {"float","double"};
@@ -120,214 +101,192 @@ public class CParser {//    Based on pages 228 and 229 in "Programming Languages
             return false;
         }
         
-        Tree.Node<CToken> newNode = new Tree.Node<CToken>(CScanner.prevToken,parentNodes.pop());
-        parseTree.insert(newNode);
-        parentNodes.push(newNode.getNodeParent()); //parent may have more children
-        parentNodes.push(newNode);
-        boolean result;
-
+        CScanner.peekNextToken(f);
         
         //IntegerType
-        if(CScanner.prevToken.token.equals("unsigned") || Arrays.asList(integerTypes).contains(CScanner.prevToken.token)){
-            result = integerType(f);
+        if(CScanner.peekedToken.token.equals("unsigned") || Arrays.asList(integerTypes).contains(CScanner.peekedToken.token)){
+            
+            if(!integerType(f)){
+                return false;
+            }
         }
        
         //FloatType
-        if(Arrays.asList(floatTypes).contains(CScanner.prevToken.token)){
-            result = floatType(f);
+        if(Arrays.asList(floatTypes).contains(CScanner.peekedToken.token)){
+            
+            if(!floatType(f)){
+                return false;
+            }
+                        
         }
         
         //InstanceType
         if(CScanner.prevToken.token.equals("instance")){
-            result = instanceType(f);
+            
+            if(!instanceType(f)){
+                return false;
+            }
+
         }
         
-        parentNodes.pop();
-        CScanner.getNextToken(f);
         return true;
     }
     
-//    public static boolean declarationType(FileInputStream f){
-//        //DeclarationType := DataType Identifier
-          // check for incorrect expected stuff
-//        Tree.Node<CToken> newNode = new Tree.Node<CToken>(CScanner.prevToken,parentNodes.pop());
-//        parseTree.insert(newNode);
-//        parentNodes.push(newNode.getNodeParent()); //parent may have more children
-//        parentNodes.push(newNode);
-//        boolean result;
-//        
-//        //DataType
-//        result = dataType(f);
-//        
-//        //Identifier
-//        result = identifier(f);
-//        
-//        CScanner.getNextToken(f);
-//        return true;
-//    }
+    public static boolean declarationType(FileInputStream f){
+        //DeclarationType := DataType Identifier
+           //check for incorrect expected stuff
+        
+        CScanner.peekNextToken(f);
+        
+        //DataType
+        if(!dataType(f)){
+            return false;
+        }
+        
+        //Identifier
+//        if(!identifier(f)){
+//            return false;
+//        }
+
+        return true;
+    }
     
 //    public static boolean identifier(FileInputStream f){
-//        // Identifier terminal
+//        // Identifier := ( _ | Alpha ) { ( _ | Digit | Alpha )  }
+//        CScanner.getNextToken(f);
 //        
 //        // Check to make sure token is legal
 //        
-//        Tree.Node<CToken> newNode = new Tree.Node<CToken>();
-//        newNode.setNodeData(CScanner.prevToken);
-//        newNode.setNodeParent(parentNodes.pop());
-//        parentNodes.push(newNode.getNodeParent()); //parent may have more children
-//        parseTree.insert(newNode);
-//        CScanner.getNextToken(f);
 //        
 //        return true;
 //    }
     
-//    public static boolean memberDeclaration(FileInputStream f){
-//        //MemberDeclaration := DeclarationType ( DataDeclaration | FunctionDeclaration)
-//        
-//        // do some checks for errors
-//        
-//        Tree.Node<CToken> newNode = new Tree.Node<CToken>(CScanner.prevToken,parentNodes.pop());
-//        parseTree.insert(newNode);
-//        parentNodes.push(newNode.getNodeParent()); //parent may have more children
-//        parentNodes.push(newNode);
-//        
-//        // DeclarationType
-//        boolean result = declarationType(f);
-//        
-//        //DataDeclaration
-//        if(CScanner.prevToken.equals(";")){
-//            result = dataDeclaration(f);
-//        }
-//        else{//FunctionDeclaration
-//            result = functionDeclaration(f);
-//        }
-//        
-//        parentNodes.pop();
-//        CScanner.getNextToken(f);
-//        
-//        return true;
-//    }
+    public static boolean memberDeclaration(FileInputStream f){
+        //MemberDeclaration := DeclarationType ( DataDeclaration | FunctionDeclaration)
+        
+        CScanner.peekNextToken(f);
+        // do some checks for errors ??
+        
+        // DeclarationType
+        if(!declarationType(f)){
+            return false;
+        }
+        
+        CScanner.peekNextToken(f);
+        
+        //DataDeclaration
+        if(CScanner.prevToken.equals(";")){
+            
+//            if(!dataDeclaration(f)){
+//                return false;
+//            }
+        }
+        else{//FunctionDeclaration
+//            if(!functionDeclaration(f)){
+//                return false;
+//            }
+        }
+        
+        return true;
+    }
     
 //    public static boolean storageDeclaration(FileInputStream f){
-////        Tree.Node<CToken> newNode = new Tree.Node<CToken>();
-////        newNode.setNodeData(CScanner.prevToken);
-////        newNode.setNodeParent(parentNodes.pop());
-////        parentNodes.push(newNode.getNodeParent()); //parent may have more children
-////        parentNodes.push(newNode);
-////        
-//        
-////        Tree.Node<CToken> newNodeChild = new Tree.Node<CToken>();
-////        newNodeChild.setNodeParent(newNode);
+//
 //        return true;
 //    }
     
 //    public static boolean mainDeclaration(FileInputStream f){
 //       
-////        Tree.Node<CToken> newNode = new Tree.Node<CToken>();
-////        newNode.setNodeData(CScanner.prevToken);
-////        newNode.setNodeParent(parentNodes.pop());
-////        parentNodes.push(newNode.getNodeParent()); //parent may have more children
-////        parentNodes.push(newNode);
-//        
-//                
-////        Tree.Node<CToken> newNodeChild = new Tree.Node<CToken>();
-////        newNodeChild.setNodeParent(newNode);
 //       return true;
 //    }
-//    
+  
     public static boolean interfaceDeclaration(FileInputStream f){
+       
+        CScanner.getNextToken(f);
         
-        boolean result = false;
-        Tree.Node<CToken> newNode = new Tree.Node<CToken>(CScanner.prevToken,parentNodes.pop());
-        parseTree.insert(newNode);
-        parentNodes.push(newNode.getNodeParent()); //parent may have more children
-        parentNodes.push(newNode);
-
         if(CScanner.prevToken.token.equals("interface")){
-            CScanner.getNextToken(f);
+            CScanner.peekNextToken(f);
             
-            if(!CScanner.prevToken.type.equals("Identifier")){
+            if(!CScanner.peekedToken.type.equals("Identifier")){
                 System.err.format("Interface Declaration rule broken @%4d .",CScanner.prevToken.lineNum);
             }
             
-//            result = identifier(f);
             
-            if(!result){
-                System.exit(0);
+//            if(!identifier(f)){
+//                return false;
+//            }
+            
+            if(!CScanner.peekNextToken(f).token.equals("{")){
+                System.err.format("Interface Declaration rule broken @%4d .",CScanner.peekedToken.lineNum);
+                return false;
+            }
+           
+            CScanner.peekNextToken(f);
+            
+            if(!memberDeclaration(f)){
+                return false;
             }
             
-            CScanner.getNextToken(f);
-            
-            if(!CScanner.prevToken.token.equals("{")){
+            if(!CScanner.peekNextToken(f).token.equals("}")){
                 System.err.format("Interface Declaration rule broken @%4d .",CScanner.prevToken.lineNum);
                 return false;
             }
             
-            newNode.addChild(new Tree.Node<CToken>(CScanner.prevToken,newNode));
-
-//            result = memberDeclaration(f);
-            
-            CScanner.getNextToken(f);
-            
-            if(!CScanner.prevToken.token.equals("}")){
-                System.err.format("Interface Declaration rule broken @%4d .",CScanner.prevToken.lineNum);
-                return false;
-            }
-            
-            newNode.addChild(new Tree.Node<CToken>(CScanner.prevToken,newNode));
-            
-            result = true;
         }
         else{ //MemberDeclaration
-//            result = memberDeclaration(f);
-            result = true;
+            
+            if(!memberDeclaration(f)){
+                return false;
+            }
         }
-        
-        parentNodes.pop(); // remove newNode from parentNodes
-        CScanner.getNextToken(f);
-        return result;
+       
+        return true;
     }
     
     public static boolean program(FileInputStream f){
 //   Program := {InterfaceDeclaration} MainDeclaration {StorageDeclaration}{ImplementationDeclaration}   
+        CScanner.peekNextToken(f); // peek at the first token
         
-        if(!CScanner.prevToken.type.equals("Keyword")){
-            System.err.format("Program rule broken @%4d .",CScanner.prevToken.lineNum);
+        if(!CScanner.peekedToken.type.equals("Keyword")){
+            System.err.format("Program rule broken @%4d .",CScanner.peekedToken.lineNum);
             return false;
         }
         
         String[] dataTypes = {"unsigned", "char","short", "int", "long", "float", "double"};
-        boolean programValid;
-        Tree.Node newNode = null;
+        boolean programValid = false;
+        
         
         //InterfaceDeclaration (optional)
-        if(CScanner.prevToken.equals("interface") || Arrays.asList(dataTypes).contains(CScanner.prevToken.token)){
+        if(CScanner.peekedToken.equals("interface") || Arrays.asList(dataTypes).contains(CScanner.peekedToken.token)){
             System.out.println("program --> interfaceDeclaration");
-            programValid = interfaceDeclaration(f);
             
+            if(!interfaceDeclaration(f)){
+                 System.exit(0);
+            }//Interface Declaration failed
         }
-        
-        if(newNode == null){
-            System.exit(0);
-        }
-                    
+             
         // MainDeclaration
         if(CScanner.prevToken.equals("void")){
-//            programValid = mainDeclaration(f);
             System.out.println("program --> mainDeclaration");
-//            if(!programValid){
+            
+//            if(!mainDeclaration(f)){
 //                System.exit(0);
 //            }
             //StorageDeclaration (optional)
             if(CScanner.prevToken.equals("storage")){
-//                programValid = storageDeclaration(f);
                 System.out.println("program --> mainDeclaration --> storage");
+//                if(!storageDeclaration(f)){
+//                    System.exit(0);
+//                }
             }
 
             //Implementation Declaration (optional)
             if(CScanner.prevToken.equals("implementation") || Arrays.asList(dataTypes).contains(CScanner.prevToken.token)){
-//                programValid = implementationDeclaration(f);
-                  System.out.println("program --> mainDeclaration--> implementationDeclaration");
+                System.out.println("program --> mainDeclaration--> implementationDeclaration");
+                
+//                if(!implementationDeclaration(f)){
+//                    System.exit(0);
+//                }
             }
            
         }
@@ -354,9 +313,6 @@ public class CParser {//    Based on pages 228 and 229 in "Programming Languages
         
         try {
             FileInputStream f = new FileInputStream(file);
-            parseTree = new Tree<CToken>(CScanner.getNextToken(f)); // Get the first token
-            parentNodes = new Stack<Tree.Node<CToken>>();
-            parentNodes.push(parseTree.getRoot());
            
             result = program(f);
             
